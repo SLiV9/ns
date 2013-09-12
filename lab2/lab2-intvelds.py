dcbf03efa604c5d17228766e8e89a89cd3926015
SLiV <sanderintveld@hotmail.com>
2013-09-12 20:20:12 +0200

    getline geeft tot CLRF, 501 en 404 werken sorta

--- lab2/lab2-intvelds.py
+++ lab2/lab2-intvelds.py
@@ -13,9 +13,12 @@ def serve(port, public_html, cgibin):
 	"""
 	print "[ start ]"
 	
+	## Get socket.
 	s = socket.socket()
 	host = socket.gethostname()
 	s.bind((host, port))
+	## Set SO_REUSEADDR to prevent "socket already in use" errors during testing.
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	
 	print "Listening as {h} on port {p}...".format(h=host, p=port)
 	
@@ -23,12 +26,40 @@ def serve(port, public_html, cgibin):
 	while True:
 	  c, addr = s.accept()
 	  print "A client at {a} connected.".format(a=addr)
-	  c.send("Hey there, farmer!")
+	  print "\t[start]"
+	  
+	  msg, leftover = getline(c, "")
+	  print "@msg@{s}@msg@".format(s=msg)
+	  print "@leftover@{s}@leftover@".format(s=leftover)
+	  
+	  method, uri, vers = msg.split(" ", 3)
+	  if (method == "GET"):
+	    if (os.path.isfile(public_html + uri)):
+	      c.send("The file exists!")
+	    else:
+	      c.send("HTTP/1.1 404 File not found.\r\n")
+	  else:
+	    c.send("HTTP/1.1 501 Method not implemented.\r\n")
+	    c.send("\r\n")
+	  
+	  print "\t[end]"
 	  c.close()
 	#.
 	
 	print "[ done ]"
 	return
+	
+def getline(c, leftover):
+  """
+  Get a full line from the client c, i.e. a string that ends at CLRF.
+  """
+  msg = leftover
+  endx = msg.find("\n")
+  while (endx < 0):
+    msg += c.recv(8)
+    endx = msg.find("\n")
+  #.
+  return msg[0:endx], msg[endx+1:]
 
 ## This the entry point of the script.
 ## Do not change this part.
